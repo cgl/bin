ec30013597693afc337b800f749626683c7de32b
Çağıl Uluşahin <cagilulusahin@gmail.com>
2019-01-07 16:45:51 +0000

    fix new reqs

--- merge_development.sh
+++ merge_development.sh
@@ -1,10 +1,12 @@
 git fetch
+
 new_requirements=false
-for filename in `git diff --name-only origin/development`; do [[ "requirements.txt" == $filename ]] && new_requirements=true ; done
+for filename in `git diff --name-only origin/development`; do [[ $filename =~ requirements/.*.txt ]] && new_requirements=true ; done
 
 git merge origin/development
 
 if $new_requirements ; then
     pip install -r requirements.txt
 fi
+
 python manage.py migrate
